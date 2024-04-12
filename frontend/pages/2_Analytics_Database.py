d7c787d379e5697247fe6941f78b283728edb643
Flemming Wu <flemming.wu@gmail.com>
2024-04-12 15:17:27 -0700

    add working table for students dimension!

--- frontend/pages/2_Analytics_Database.py
+++ frontend/pages/2_Analytics_Database.py
@@ -16,7 +16,43 @@ with student_tab:
     with st.container():
         st.header('Table: **Students Dimension** :male-student: :female-student:', divider='gray')
         query = f"""
-        SELECT * FROM '/data_lake/students/data/*.parquet'; 
+        WITH current_row_indicator_added AS (
+            SELECT *,
+                CASE WHEN source_datetime = max_source_datetime THEN 'yes' ELSE 'no' END AS current_row_indicator
+            FROM  (
+                SELECT *,
+                    MAX(source_datetime) OVER (PARTITION BY id) AS max_source_datetime
+                FROM '/data_lake/students/data/*.parquet'
+            ) temp
+        ),
+        end_effective_date_added AS (
+        SELECT *,
+            CASE
+                WHEN current_row_indicator = 'yes' then '9999-12-30'
+                ELSE LEAD(source_datetime) OVER (PARTITION BY id ORDER BY source_datetime)
+            END AS effective_date_end
+        FROM current_row_indicator_added
+        ),
+        final AS (
+            SELECT
+                id,
+                first_name,
+                last_name,
+                gender,
+                address,
+                date_of_birth,
+                major,
+                year_of_study,
+                gpa,
+                enrollment_status,
+                source_datetime::timestamp AS effective_date_start,
+                effective_date_end::timestamp - interval 1 second AS effective_date_end,
+                current_row_indicator
+            FROM end_effective_date_added
+            ORDER BY id
+        )
+        SELECT *
+        FROM final
         """
         table = duckdb.sql(query=query).df()
         st.dataframe(
