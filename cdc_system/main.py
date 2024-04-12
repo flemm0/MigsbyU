735d4993356c80b886bb9ec8fecb8089a8e10252
Flemming Wu <flemming.wu@gmail.com>
2024-04-12 14:04:50 -0700

    turn off test settings in cdc system

--- cdc_system/main.py
+++ cdc_system/main.py
@@ -6,7 +6,7 @@ from utils import create_spark_connection
 
 if __name__ == '__main__':
     spark = create_spark_connection()
-    students_streaming_query = students_streaming_query(spark, testing=True)
-    professsors_streaming_query = professsors_streaming_query(spark, testing=True)
-    courses_streaming_query = courses_streaming_query(spark, testing=True)
+    students_streaming_query = students_streaming_query(spark)
+    professsors_streaming_query = professsors_streaming_query(spark)
+    courses_streaming_query = courses_streaming_query(spark)
     spark.streams.awaitAnyTermination()
